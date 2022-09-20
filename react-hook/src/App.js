2e30a8399b84078002742e02332840fdbbdbae68
priliaaprilia08 <apriliapriliarahmawatia@gmail.com>
2022-09-20 17:34:15 +0700

    tugas reacthook

---
+++ react-hook/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Todos from './components/Todos';
+
+function App() {
+    return( 
+      <div>
+        <Todos />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+      </div>
+    );
+}
+
+export default App;
